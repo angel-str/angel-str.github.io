577ab72b8216b46a7e166c90bf1b4d16357af7ca
angel-str <60510865+angel-str@users.noreply.github.com>
2021-02-15 10:14:19 +0100

    intro

--- EgenHemsida1/js/app.js
+++ EgenHemsida1/js/app.js
@@ -4,3 +4,9 @@ Idéer:
  *"Intro" / "Loadingscreen"
 
 */
+const t1 = gsap.timeline({defaults: {ease: "power1.out"}});
+
+t1.to("#s1", {x: "100%", duration: "0.6", delay: "1.5"});
+t1.to("#s2 span", {y: "0", duration: "1", stagger: "0.25"});
+t1.to("#s2 span", {color: "white", duration: "1", stagger: "0.25"}, "-=1");
+t1.to("#s2", {y: "-100%", duration: "0.6", delay: "0.5"});
