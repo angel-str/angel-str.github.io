8d0b3c2e0ac8aaa8c0d852325edc5c1520e0364e
angel-str <60510865+angel-str@users.noreply.github.com>
2021-02-15 10:17:46 +0100

    ??

--- EgenHemsida1/js/app.js
+++ EgenHemsida1/js/app.js
@@ -6,7 +6,7 @@ Idéer:
 */
 const t1 = gsap.timeline({defaults: {ease: "power1.out"}});
 
-t1.to("#s1", {x: "100%", duration: "0.6", delay: "1.5"});
+t1.to("#s1", {x: "120%", duration: "0.6", delay: "1.5"});
 t1.to("#s2 span", {y: "0", duration: "1", stagger: "0.25"});
 t1.to("#s2 span", {color: "white", duration: "1", stagger: "0.25"}, "-=1");
 t1.to("#s2", {y: "-100%", duration: "0.6", delay: "0.5"});
