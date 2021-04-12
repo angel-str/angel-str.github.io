f5ebed4d9c9f428682c9863252c787eda548b56d
angel-str <60510865+angel-str@users.noreply.github.com>
2021-04-12 10:15:39 +0200

    Create main.js

---
+++ ANAcopy/main.js
@@ -0,0 +1,50 @@
+
+$(document).ready(function(){
+    $('.btn').css({
+      "display":"inline"
+    });
+    $('.slider').css({
+        "border": "12px solid  #7B0000",
+        "overflow": "hidden"
+    });
+    $('.pictures').css({
+        "display": "flex"
+  
+    });
+  });
+  
+  const fKnapp= document.querySelector('#förra');
+  const nKnapp= document.querySelector('#nästa');
+  
+  var currentPic = 1;
+  
+  nKnapp.addEventListener('click',()=>{
+      if(currentPic == 3){
+          $('.pictures').animate({
+              "marginLeft": "+=1280px"
+          })
+          currentPic = 1;
+      }else{
+          $('.pictures').animate({
+              "marginLeft": "-=640px"
+          })
+          currentPic++;
+      }
+  
+  
+  });
+  
+  
+  fKnapp.addEventListener('click',()=>{
+    if(currentPic == 1){
+        $('.pictures').animate({
+            "marginLeft": "-=1280px"
+        })
+        currentPic = 3;
+    }else{
+        $('.pictures').animate({
+            "marginLeft": "+=640px"
+        })
+        currentPic--;
+    };
+});
