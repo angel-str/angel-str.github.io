48baea6e005af094ff8ad5de8e817ba08d348aca
angel-str <60510865+angel-str@users.noreply.github.com>
2021-05-18 09:16:03 +0200

    phil

--- CV/js/app.js
+++ CV/js/app.js
@@ -1,3 +1,16 @@
 console.log("Connected");
-console.log(document.getElementById("jsHide"));
-document.getElementsByClassName("jsHide").style.display = "none";
+var jsHide_var = [];
+jsHide_var = document.getElementsByClassName("jsHide");
+var jsShow_var = [];
+jsShow_var = document.getElementsByClassName("jsShow");
+
+console.log(jsHide_var);
+
+jsHide_var[0].style.display = "none";
+
+function onScroll(){
+  var y = window.scrollY;
+  document.getElementById("listSvg");
+}
+
+window.addEventListener("scroll", onScroll);
